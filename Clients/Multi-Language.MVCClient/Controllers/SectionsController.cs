a25ad2ffd12fd949aaf3fea49fa27a0533680831
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-10-12 09:49:11 +0300

    Added sections. Now when the body is rendered the sections can be combined and rendered later. FInd a better way to make ajax loader in the view if necessary

---
+++ Clients/Multi-Language.MVCClient/Controllers/SectionsController.cs
@@ -0,0 +1,24 @@
+﻿using Multi_Language.MVCClient.Models.SectionsViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Multi_Language.MVCClient.Controllers
+{
+    public class SectionsController : BaseController
+    {
+        // GET: Sections
+        public async Task<ActionResult> FirstRow(string id)
+        {
+            if (id == "Resources")
+            {
+                var model = new ResourcesFirstRowSectionViewModel();
+                return PartialView("ResourcesFirstRowSection");
+            }
+            return PartialView("Default");
+        }
+    }
+}
