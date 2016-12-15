44fc147e2075306bd8a8307fc86fee91715c0852
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-12-15 16:54:32 +0200

    Added backup functionalities to admin UI

---
+++ Admin/Multi-Language.MVCClient/Controllers/Utilities/BackupsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Multi_Language.MVCClient.Models.UtilitiesViewModels;
+using Newtonsoft.Json;
+
+namespace Multi_Language.MVCClient.Controllers.Utilities
+{
+    public class BackupsController : BaseController
+    {
+        // GET: Backup
+        public ActionResult Index()
+        {
+            var model = new List<BackupViewModels>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync(ConfigurationManager.AppSettings["MultiLanguageApiUrl"] + "/backup/getall").GetAwaiter().GetResult();
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    model = JsonConvert.DeserializeObject<List<BackupViewModels>>(result);
+                }
+            }
+            SetViewBagsAndHeaders(Request.IsAjaxRequest(), "Backups page", "Create, upload and restore the DataBase");
+            if (Request.IsAjaxRequest())
+                return PartialView(model);
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Admin/Multi-Language.MVCClient/Models/UtilitiesViewModels/BackupViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multi_Language.MVCClient.Models.UtilitiesViewModels
+{
+    public class BackupViewModels
+    {
+        public string FileName { get; set; }
+
+        public string FileSize { get; set; }
+    }
+}
