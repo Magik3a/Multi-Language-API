04ac73a33d028c8b49bff75aaf5552f4a6ae5b0c
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2017-01-31 21:09:46 +0200

    Added home screen poppup for mobile devices, fixed some bugs on android style app and slideUp/Down header logo for mobile devices

--- Admin/Multi-Language.MVCClient/Scripts/MltApi/MltApi-LayoutHelpers.js
+++ Admin/Multi-Language.MVCClient/Scripts/MltApi/MltApi-LayoutHelpers.js
@@ -112,6 +112,54 @@ $.MltApi.LoadProjectBox = function () {
     });
 };
 
+
+$.MltApi.HideHomeLogoText = function () {
+    // CHECK if mobile
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // Hide Header on on scroll down
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $('#home-logo').outerHeight();
+
+
+        $(window).scroll(function(event) {
+            didScroll = true;
+        });
+
+        setInterval(function() {
+                if (didScroll) {
+                    hasScrolled();
+                    didScroll = false;
+                }
+            },
+            250);
+
+        function hasScrolled() {
+            var st = $(this).scrollTop();
+
+            // Make sure they scroll more than delta
+            if (Math.abs(lastScrollTop - st) <= delta)
+                return;
+
+            // If they scrolled down and are past the navbar, add class .nav-up.
+            // This is necessary so you never see what is "behind" the navbar.
+            if (st > lastScrollTop && st > navbarHeight) {
+                // Scroll Down
+                //$('#home-logo').removeClass('nav-down').addClass('nav-up');
+                $('#home-logo').slideUp(500);
+            } else {
+                // Scroll Up
+                if (st + $(window).height() < $(document).height()) {
+                    //$('#home-logo').removeClass('nav-up').addClass('nav-down');
+                    $('#home-logo').slideDown(500);
+                }
+            }
+
+            lastScrollTop = st;
+        }
+    }
+};
 window.onpopstate = function (e) {
     if (e.state) {
         $(".wrapper").html(e.state.html);
